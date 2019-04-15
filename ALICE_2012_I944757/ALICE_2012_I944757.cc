3a2020568127c0fc88f7abe54e4d5f4e1612dcad
jackal1-66 <mgiacalo@gmail.com>
2019-04-15 13:49:04 +0200

    Adding ALICE_2012_I944757 paper to RIVET

---
+++ ALICE_2012_I944757/ALICE_2012_I944757.cc
@@ -0,0 +1,91 @@
+// -*- C++ -*-
+#include "Rivet/Analysis.hh"
+#include "Rivet/Projections/UnstableFinalState.hh"
+#include "Rivet/Projections/FastJets.hh"
+
+namespace Rivet {
+
+
+  /// @brief Add a short analysis description here
+  class ALICE_2012_I944757 : public Analysis {
+  public:
+
+    /// Constructor
+    DEFAULT_RIVET_ANALYSIS_CTOR(ALICE_2012_I944757);
+
+
+    /// @name Analysis methods
+    //@{
+
+    /// Book histograms and initialise projections before the run
+    void init() {
+
+      // Initialise and register projections
+      declare(UnstableFinalState(Cuts::absrap < 0.5), "UFS");
+      
+
+      // Book histograms
+      _h_D0 = bookHisto1D(1, 1, 1);
+      _h_Dplus = bookHisto1D(2, 1, 1);
+      _h_Dstarp= bookHisto1D(3, 1, 1);
+      _h_integ = bookHisto1D(4, 1, 1);
+
+    }
+
+
+    /// Perform the per-event analysis
+    void analyze(const Event& event) {
+        const double weight = event.weight();
+        const UnstableFinalState& ufs = apply<UnstableFinalState>(event, "UFS");
+        
+        foreach (const Particle& p, ufs.particles()) {
+            if(p.fromBottom())
+                continue;
+            else
+                {    
+                if(p.abspid() == 421){
+                    _h_D0->fill(p.pT()/GeV, weight); 
+                    _h_integ->fill(1,weight);}
+                else if(p.abspid() == 411){
+                    _h_Dplus->fill(p.pT()/GeV, weight);
+                    _h_integ->fill(2,weight);}
+                else if(p.abspid()== 413){
+                    _h_Dstarp->fill(p.pT()/GeV, weight);    
+                    _h_integ->fill(3,weight);}
+                }
+        }
+        
+
+    }
+
+
+    /// Normalise histograms etc., after the run
+    void finalize() {
+
+      //normalize(_h_YYYY); // normalize to unity
+      scale(_h_D0, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+      scale(_h_Dplus, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+      scale(_h_Dstarp, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+      scale(_h_integ, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+
+    }
+
+    //@}
+
+
+    /// @name Histograms
+    //@{
+    Histo1DPtr _h_D0, _h_Dplus, _h_Dstarp, _h_integ;
+    //Profile1DPtr _p_AAAA;
+    //CounterPtr _c_BBBB;
+    //@}
+
+
+  };
+
+
+  // The hook for the plugin system
+  DECLARE_RIVET_PLUGIN(ALICE_2012_I944757);
+
+
+}
---
+++ ALICE_2012_I944757/ALICE_2012_I944757.plot
@@ -0,0 +1,49 @@
+BEGIN PLOT /ALICE_2012_I944757/d01-x01-y01
+Title=Differential cross section for prompt $D^0$ production.
+XLabel=$p_T$ [GeV/c]
+#PlotSize=10,8
+#LegendYPos=0.96
+#LegendXPos=0.55
+YLabel=$\frac{\text{d}\sigma}{\text{d}p_T}$ [$\mu$b/GeV]
+ConnectBins=0
+RatioPlotYMax=1.8
+# + any additional plot settings you might like, see make-plots documentation
+END PLOT
+
+BEGIN PLOT /ALICE_2012_I944757/d02-x01-y01
+Title=Differential cross section for prompt $D^+$ production.
+XLabel=$p_T$ [GeV/c]
+YLabel=$\frac{\text{d}\sigma}{\text{d}p_T}$ [$\mu$b/GeV]
+ConnectBins=0
+#PlotSize=10,8
+#LegendYPos=0.96
+#LegendXPos=0.55
+RatioPlotYMax=1.8
+# + any additional plot settings you might like, see make-plots documentation
+END PLOT
+
+BEGIN PLOT /ALICE_2012_I944757/d03-x01-y01
+Title=Differential cross section for prompt $D^{*+}$ production.
+XLabel=$p_T$ [GeV/c]
+YLabel=$\frac{\text{d}\sigma}{\text{d}p_T}$ [$\mu$b/GeV]
+ConnectBins=0
+#PlotSize=10,8
+#LegendYPos=0.96
+#LegendXPos=0.55
+RatioPlotYMax=1.8
+# + any additional plot settings you might like, see make-plots documentation
+END PLOT
+
+BEGIN PLOT /ALICE_2012_I944757/d04-x01-y01
+Title=Integrated cross sections for prompt charm particle production. 
+XLabel=pp\to D$^0$ X;\qquad \qquad \qquad pp\to D$^+$ X;\qquad \qquad \qquad pp\to D$^{*+}$ X. 
+YLabel=$\frac{\text{d}\sigma}{\text{d}y}$ [$\mu$b]
+ConnectBins=0
+#RatioPlotYMin=0.1
+#PlotSize=10,8
+LegendYPos=0.95
+LegendXPos=0.55
+# + any additional plot settings you might like, see make-plots documentation
+END PLOT
+
+# ... add more histograms as you need them ...
