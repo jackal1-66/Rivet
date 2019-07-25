079acbbfae958726e17fc69e4f9f532d1bd6aeff
jackal1-66 <mgiacalo@gmail.com>
2019-07-25 09:55:54 +0200

    Solved scaling issue using rivet-merge

--- ALICE_2017_I1645239/ALICE_2017_I1645239.cc
+++ ALICE_2017_I1645239/ALICE_2017_I1645239.cc
@@ -45,8 +45,7 @@ namespace Rivet {
       _h_D0intPb = bookHisto1D("D0intPb", 1, -0.6, -0.4 , "D0 int Pb");
       _h_LcR = bookHisto1D("LcR", binEdges2, "Lc R");
       _h_LcRPb = bookHisto1D("LcRPb", binEdges2, "Lc RPb");
-      //_h_cross = bookHisto1D("_h_cross",1,0,2, "cross test");
-      bo1 = bo2 = bo3 = false;
+      // bo1 = bo2 = bo3 = false;
      }
 
 
@@ -60,7 +59,7 @@ namespace Rivet {
         /*PDG code IDs used inside the foreach cycle: 421 = D0, 411 = D+, 413 = D*+ */
       if(beamp.first == 2212 && beamp.second ==2212){
         if(fuzzyEquals(sqrtS()/GeV,5000)){
-         bo2 = true;
+         //bo2 = true;
          foreach (const Particle& p, ufs.particles()) {
             if(p.fromBottom())
                 continue;
@@ -74,7 +73,7 @@ namespace Rivet {
         } 
         }}
        else{
-         bo1 = true;
+         //bo1 = true;
          foreach (const Particle& p, ufs.particles()) {
             if(p.fromBottom())
                 continue;
@@ -96,7 +95,7 @@ namespace Rivet {
         //const GenEvent * evgen = event.genEvent();
         //double crossx = evgen->cross_section()->cross_section();
         //_h_cross->fill(1,crossx/microbarn);
-        bo3 = true;
+        //bo3 = true;
         foreach (const Particle& p, ufs.particles()) {
             if(p.fromBottom())
                 continue;
@@ -104,14 +103,14 @@ namespace Rivet {
                 {    
                  if(p.rap() < 0.04 && p.rap() > -0.96){
                      if(p.abspid() == 421){
-                         _h_D0Pb->fill(p.pT()/GeV, 1); 
-                         _h_D0intPb->fill(-0.5,1);
+                         _h_D0Pb->fill(p.pT()/GeV, weight); 
+                         _h_D0intPb->fill(-0.5,weight);
 			 }
                      else if(p.abspid() == 4122){
-                         _h_LcPb->fill(p.pT()/GeV, 1);
-			 _h_LcPbdummy->fill(p.pT()/GeV, 1);
-                         _h_LcRPb->fill(p.pT()/GeV, 1);
-                         _h_LcintPb->fill(-0.5,1);
+                         _h_LcPb->fill(p.pT()/GeV, weight);
+			 _h_LcPbdummy->fill(p.pT()/GeV, weight);
+                         _h_LcRPb->fill(p.pT()/GeV, weight);
+                         _h_LcintPb->fill(-0.5,weight);
 			 }
                      }    
                 }    
@@ -123,22 +122,22 @@ namespace Rivet {
 
     /// Normalise histograms etc., after the run
     void finalize() {
-      if(bo1 == true) scale(_h_D0, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
-      if(bo1 == true) scale(_h_D0int, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
-      if(bo1 == true) scale(_h_Lc, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
-      if(bo1 == true) scale(_h_Lcdummy, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
-      if(bo3 == true) scale(_h_LcPbdummy, crossSection()/(microbarn*2*numEvents())); // norm to cross section
-      if(bo1 == true) scale(_h_Lcint, crossSection()/(microbarn*2*sumOfWeights())); //norm to cross section
-      if(bo3 == true) scale(_h_D0Pb, crossSection()/(microbarn*2*numEvents())); // norm to cross section
-      if(bo3 == true) scale(_h_D0intPb, crossSection()/(microbarn*2*numEvents())); // norm to cross section
-      if(bo3 == true) scale(_h_LcPb, crossSection()/(microbarn*2*numEvents())); // norm to cross section
-      if(bo3 == true) scale(_h_LcintPb, crossSection()/(microbarn*2*numEvents())); //norm to cross section
+      if(_h_D0->numEntries()>0) scale(_h_D0, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+      if(_h_D0int->numEntries()>0) scale(_h_D0int, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+      if(_h_Lc->numEntries()>0) scale(_h_Lc, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+      if(_h_Lcdummy->numEntries()>0) scale(_h_Lcdummy, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+      if(_h_LcPbdummy->numEntries()>0) scale(_h_LcPbdummy, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+      if(_h_Lcint->numEntries()>0) scale(_h_Lcint, crossSection()/(microbarn*2*sumOfWeights())); //norm to cross section
+      if(_h_D0Pb->numEntries()>0) scale(_h_D0Pb, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+      if(_h_D0intPb->numEntries()>0) scale(_h_D0intPb, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+      if(_h_LcPb->numEntries()>0) scale(_h_LcPb, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+      if(_h_LcintPb->numEntries()>0) scale(_h_LcintPb, crossSection()/(microbarn*2*sumOfWeights())); //norm to cross section
       if (_h_Lcdummy->numEntries()>0 && _h_D0->numEntries()>0) divide(_h_Lcdummy, _h_D0, _h_LcD0);
       if (_h_LcPbdummy->numEntries()>0 && _h_D0Pb->numEntries()>0) divide(_h_LcPbdummy, _h_D0Pb, _h_LcD0Pb);
       if (_h_Lcint->numEntries()>0 && _h_D0int->numEntries()>0) divide(_h_Lcint, _h_D0int, _h_LcD0int);
       if (_h_LcintPb->numEntries()>0 && _h_D0intPb->numEntries()>0) divide(_h_LcintPb, _h_D0intPb, _h_LcD0Pbint);
-      if(bo2 == true) scale(_h_LcR, 208*crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
-      if(bo3 == true) scale(_h_LcRPb, crossSection()/(microbarn*2*numEvents())); // norm to cross section
+      if(_h_LcR->numEntries()>0) scale(_h_LcR, 208*crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+      if(_h_LcRPb->numEntries()>0) scale(_h_LcRPb, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
       if (_h_LcRPb->numEntries()>0 && _h_LcR->numEntries()>0) divide(_h_LcRPb, _h_LcR, _h_RpPb);
     }
 
@@ -149,7 +148,7 @@ namespace Rivet {
     //@{
     Histo1DPtr _h_Lc, _h_LcPb, _h_D0, _h_D0Pb, _h_Lcint, _h_LcintPb, _h_D0int, _h_D0intPb, _h_LcR, _h_LcRPb, _h_Lcdummy, _h_LcPbdummy;
     Scatter2DPtr _h_LcD0, _h_LcD0Pb, _h_LcD0int,  _h_LcD0Pbint, _h_RpPb;
-    bool bo1, bo2, bo3;
+    //bool bo1, bo2, bo3;
     //@}
 
 
