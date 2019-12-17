5e442272dedcc8cc7050e3b48911335c9dd97e1c
Marco Giacalone <mgiacalo@gmail.com>
2019-12-17 16:21:48 +0100

    Added archive for Lc rivetization + added Antonin comments to D mesons rivetization

--- ALICE_2017_I1511870/ALICE_2017_I1511870.cc
+++ ALICE_2017_I1511870/ALICE_2017_I1511870.cc
@@ -23,26 +23,26 @@ namespace Rivet {
       declare(UnstableParticles(), "UFS");
 
       // Book histograms
-      _h_D0         = bookHisto1D(1, 1, 1);                                   //production cross section of D0 meson
-      _h_Dplus      = bookHisto1D(2, 1, 1);                                   //production cross section of Dplus meson
-      _h_Dstar      = bookHisto1D(3, 1, 1);                                   //production cross section of Dstar meson
-      _h_Ds         = bookHisto1D(4, 1, 1);                                   //production cross section of Ds meson
-      _h_DplusonD0  = bookScatter2D(5, 1, 1);                                 //ratio of production cross section between Dplus and D0
-      _h_DstaronD0  = bookScatter2D(6, 1, 1);                                 //ratio of production cross section between Dstar and D0
-      _h_DsonD0     = bookScatter2D(7, 1, 1);                                 //ratio of production cross section between Ds and D0
-      _h_DsonDplus  = bookScatter2D(8, 1, 1);                                 //ratio of production cross section between Ds and Dplus
-      _h_D0int      = bookHisto1D(9,1,1);                                     //pt-integrated production cross section of D0 meson
-      _h_Dplusint   = bookHisto1D(10,1,1);                                    //pt-integrated production cross section of Dplus meson
-      _h_Dstarint   = bookHisto1D(11,1,1);                                    //pt-integrated production cross section of Dstar meson
-      _h_Dsint      = bookHisto1D(12,1,1);                                    //pt-integrated production cross section of Ds meson
-      _h_cc         = bookHisto1D(13,1,1);                                    //integrated cross section of ccbar
-      _h_ptd0       = bookHisto1D(14,1,1);                                    //mean pt of prompt D0
-      _h_D0full     = bookHisto1D(15,1,1);                                    //integrated cross section for D0 in full rapidity
-      _h_ccfull     = bookHisto1D(16,1,1);                                    //integrated cross section for ccbar in full rapidity
-      _h_wei        = bookHisto1D("_h_wei",1,6999.5,7000.5, "Weight dummy");  //histograms used to calculate D0 mean pt
-      _h_D0dummy    = bookHisto1D("TMP/_h_D0dummy", refData(5,1,1));          //used to make DplusonD0
-      _h_D0dummy1   = bookHisto1D("TMP/_h_D0dummy1", refData(7,1,1));         //used to make DsonD0
-      _h_Dplusdummy = bookHisto1D("TMP/_h_Dplusdummy", refData(8,1,1));       //used to make DsonDplus
+      _h_D0         = bookHisto1D(1, 1, 1);                                   // production cross section of D0 meson
+      _h_Dplus      = bookHisto1D(2, 1, 1);                                   // production cross section of Dplus meson
+      _h_Dstar      = bookHisto1D(3, 1, 1);                                   // production cross section of Dstar meson
+      _h_Ds         = bookHisto1D(4, 1, 1);                                   // production cross section of Ds meson
+      _h_DplusonD0  = bookScatter2D(5, 1, 1);                                 // ratio of production cross section between Dplus and D0
+      _h_DstaronD0  = bookScatter2D(6, 1, 1);                                 // ratio of production cross section between Dstar and D0
+      _h_DsonD0     = bookScatter2D(7, 1, 1);                                 // ratio of production cross section between Ds and D0
+      _h_DsonDplus  = bookScatter2D(8, 1, 1);                                 // ratio of production cross section between Ds and Dplus
+      _h_D0int      = bookHisto1D(9,1,1);                                     // pt-integrated production cross section of D0 meson
+      _h_Dplusint   = bookHisto1D(10,1,1);                                    // pt-integrated production cross section of Dplus meson
+      _h_Dstarint   = bookHisto1D(11,1,1);                                    // pt-integrated production cross section of Dstar meson
+      _h_Dsint      = bookHisto1D(12,1,1);                                    // pt-integrated production cross section of Ds meson
+      _h_cc         = bookHisto1D(13,1,1);                                    // integrated cross section of ccbar
+      _h_ptd0       = bookHisto1D(14,1,1);                                    // mean pt of prompt D0
+      _h_D0full     = bookHisto1D(15,1,1);                                    // integrated cross section for D0 in full rapidity
+      _h_ccfull     = bookHisto1D(16,1,1);                                    // integrated cross section for ccbar in full rapidity
+      _h_wei        = bookHisto1D("_h_wei",1,6999.5,7000.5, "Weight dummy");  // histogram used to calculate D0 mean pt (necessary to store an integer number for event normalisation, i.e. to still be able to compute <pT> from multiple parallel MC jobs)
+      _h_D0dummy    = bookHisto1D("TMP/_h_D0dummy",     refData(5,1,1));      // used to make DplusonD0
+      _h_D0dummy1   = bookHisto1D("TMP/_h_D0dummy1",    refData(7,1,1));      // used to make DsonD0
+      _h_Dplusdummy = bookHisto1D("TMP/_h_Dplusdummy",  refData(8,1,1));      // used to make DsonDplus
     }
 
 
@@ -55,34 +55,35 @@ namespace Rivet {
         foreach (const Particle& p, ufs.particles()) {
           if(p.abspid() == 421){
               if(not p.fromBottom()){
-                  _h_D0full->fill(7.000000e+03/GeV,weight);    
-                  _h_ccfull->fill(7.000000e+03/GeV,weight);}}
+                  _h_D0full         ->fill(7.000000e+03/GeV,weight);    
+                  _h_ccfull         ->fill(7.000000e+03/GeV,weight);}
+          }
           if(p.absrap() < 0.5){
             if(p.fromBottom())
                 continue;
             else
                 {    
                 if(p.abspid() == 421){
-                    _h_D0->fill(p.pT()/GeV, weight); 
-                    _h_D0dummy->fill(p.pT()/GeV, weight);
-                    _h_D0dummy1->fill(p.pT()/GeV, weight);
-                    _h_D0int->fill(7.000000e+03/GeV, weight);
-                    _h_cc->fill(7.000000e+03/GeV, weight);
-                    _h_ptd0->fill(7.000000e+03/GeV,p.pT()/GeV,weight);
-                    _h_wei->fill(7.000000e+03/GeV,weight);
+                    _h_D0           ->fill(p.pT()/GeV, weight); 
+                    _h_D0dummy      ->fill(p.pT()/GeV, weight);
+                    _h_D0dummy1     ->fill(p.pT()/GeV, weight);
+                    _h_D0int        ->fill(7.000000e+03/GeV, weight);
+                    _h_cc           ->fill(7.000000e+03/GeV, weight);
+                    _h_ptd0         ->fill(7.000000e+03/GeV,p.pT()/GeV,weight);
+                    _h_wei          ->fill(7.000000e+03/GeV,weight);
                     }
                 else if(p.abspid() == 411){
-                    _h_Dplus->fill(p.pT()/GeV, weight);
-                    _h_Dplusdummy->fill(p.pT()/GeV, weight);
-                    _h_Dplusint->fill(7.000000e+03/GeV, weight); 
+                    _h_Dplus        ->fill(p.pT()/GeV, weight);
+                    _h_Dplusdummy   ->fill(p.pT()/GeV, weight);
+                    _h_Dplusint     ->fill(7.000000e+03/GeV, weight); 
                     }
                 else if(p.abspid() == 413){
-                    _h_Dstar->fill(p.pT()/GeV, weight); 
-                    _h_Dstarint->fill(7.000000e+03/GeV, weight);    
+                    _h_Dstar        ->fill(p.pT()/GeV, weight); 
+                    _h_Dstarint     ->fill(7.000000e+03/GeV, weight);    
                     }
                 else if(p.abspid() == 431){
-                    _h_Ds->fill(p.pT()/GeV, weight);
-                    _h_Dsint->fill(7.000000e+03/GeV, weight); 
+                    _h_Ds           ->fill(p.pT()/GeV, weight);
+                    _h_Dsint        ->fill(7.000000e+03/GeV, weight); 
                 }    
                 }
         }
@@ -94,25 +95,36 @@ namespace Rivet {
 
       //normalize(_h_YYYY); // normalize to unity
       //all the histograms are divided by 2 since at this point the results consider both particles and antiparticles
-      scale(_h_D0, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
-      scale(_h_Dplus, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
-      scale(_h_Dstar, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
-      scale(_h_Ds, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
-      scale(_h_D0dummy, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
-      scale(_h_D0dummy1, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
-      scale(_h_Dplusdummy, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
-      divide(_h_Dplus,_h_D0dummy,_h_DplusonD0); //ratio plots
-      divide(_h_Dstar,_h_D0dummy,_h_DstaronD0);
-      divide(_h_Ds,_h_D0dummy1,_h_DsonD0);
-      divide(_h_Ds,_h_Dplusdummy,_h_DsonDplus);
-      scale(_h_D0int, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
-      scale(_h_Dplusint, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
-      scale(_h_Dstarint, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
-      scale(_h_Dsint, crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
-      scale(_h_cc, 1.034*crossSection()/(microbarn*2*sumOfWeights()*0.542)); //0.542 (with an error of 0.024) accounts for the fraction of charm quarks hadronizing into D0 mesons, 1.034 (1.5% error) is used to include the correction of the different shapes of the rapidity distributions of D0 and ccbar 
-      scale(_h_ptd0, 1/_h_wei->bin(0).area()); //bin(0).area() returns the number of events used to calculate the mean pT
-      scale(_h_D0full,crossSection()/(millibarn*2*sumOfWeights()));
-      scale(_h_ccfull, 1.034*crossSection()/(millibarn*2*sumOfWeights()*0.542)); //0.542 (with an error of 0.024) accounts for the fraction of charm quarks hadronizing into D0 mesons, 1.034 (1.5% error) used to include the correction of the different shapes of the rapidity distributions of D0 and ccbar
+      scale(_h_D0,          crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+      scale(_h_Dplus,       crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+      scale(_h_Dstar,       crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+      scale(_h_Ds,          crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+      scale(_h_D0dummy,     crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+      scale(_h_D0dummy1,    crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+      scale(_h_Dplusdummy,  crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+      
+      divide(_h_Dplus,  _h_D0dummy,     _h_DplusonD0); //ratio plots
+      divide(_h_Dstar,  _h_D0dummy,     _h_DstaronD0);
+      divide(_h_Ds,     _h_D0dummy1,    _h_DsonD0);
+      divide(_h_Ds,     _h_Dplusdummy,  _h_DsonDplus);
+      
+      scale(_h_D0int,       crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+      scale(_h_Dplusint,    crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+      scale(_h_Dstarint,    crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+      scale(_h_Dsint,       crossSection()/(microbarn*2*sumOfWeights())); // norm to cross section
+      
+      scale(_h_cc,          1.034*crossSection()/(microbarn*2*sumOfWeights()*0.542)); 
+            // 0.542 accounts for the fraction of charm quarks hadronizing into D0 mesons, the ALICE uncertainty on such a factor (0.542 ± 0.024) is not propagated here.
+            // 1.034 is used to include the correction of the different shapes of the rapidity distributions of D0 and ccbar, the ALICE uncertainty on such a factor (1.034 ± 0.015, i.e. 1.5 percentage point of uncertainty) is not propagated here.
+      
+      scale(_h_ptd0,        1/_h_wei->bin(0).area()); //bin(0).area() returns the number of events used to calculate the mean pT
+      
+      scale(_h_D0full,      crossSection()/(millibarn*2*sumOfWeights())); 
+            // NOTE : for the ALICE data, the y extrapolation from |y|<0.5 to full-y phase space is done with an FONLL-based factor (8.56 +2.51 -0.42). Here in MC we simply use direct MC outcome over full y.
+      
+      scale(_h_ccfull,      1.034*crossSection()/(millibarn*2*sumOfWeights()*0.542)); 
+            // 0.542 accounts for the fraction of charm quarks hadronizing into D0 mesons, the ALICE uncertainty on such a factor (0.542 ± 0.024) is not propagated here.
+            // 1.034 is used to include the correction of the different shapes of the rapidity distributions of D0 and ccbar, the ALICE uncertainty on such a factor (1.034 ± 0.016, i.e. 1.5 percentage point of uncertainty) is not propagated here.
     }
 
     //@}
--- ALICE_2017_I1511870/ALICE_2017_I1511870.plot
+++ ALICE_2017_I1511870/ALICE_2017_I1511870.plot
@@ -1,5 +1,5 @@
 BEGIN PLOT /ALICE_2017_I1511870/d01-x01-y01
-Title=Differential cross section for prompt $D^0$ production.
+Title=Differential cross section for prompt $D^0$ production ($|y|<0.5$).
 XLabel=$p_T$ [GeV/c]
 #PlotSize=10,8
 #LegendYPos=0.96
@@ -11,7 +11,7 @@ RatioPlotYMax=1.5
 END PLOT
 
 BEGIN PLOT /ALICE_2017_I1511870/d02-x01-y01
-Title=Differential cross section for prompt $D^+$ production.
+Title=Differential cross section for prompt $D^+$ production ($|y|<0.5$).
 XLabel=$p_T$ [GeV/c]
 YLabel=$\frac{\text{d}\sigma}{\text{d}p_T}$ [$\mu$b/GeV]
 ConnectBins=0
@@ -23,7 +23,7 @@ RatioPlotYMax=1.5
 END PLOT
 
 BEGIN PLOT /ALICE_2017_I1511870/d03-x01-y01
-Title=Differential cross section for prompt $D^{*+}$ production.
+Title=Differential cross section for prompt $D^{*+}$ production ($|y|<0.5$).
 XLabel=$p_T$ [GeV/c]
 YLabel=$\frac{\text{d}\sigma}{\text{d}p_T}$ [$\mu$b/GeV]
 ConnectBins=0
@@ -35,7 +35,7 @@ RatioPlotYMax=1.5
 END PLOT
 
 BEGIN PLOT /ALICE_2017_I1511870/d04-x01-y01
-Title=Differential cross section for prompt $D_{s}^+$ production.
+Title=Differential cross section for prompt $D_{s}^+$ production ($|y|<0.5$).
 XLabel=$p_T$ [GeV/c]
 YLabel=$\frac{\text{d}\sigma}{\text{d}p_T}$ [$\mu$b/GeV]
 ConnectBins=0
@@ -47,7 +47,7 @@ LegendXPos=0.55
 END PLOT
 
 BEGIN PLOT /ALICE_2017_I1511870/d05-x01-y01
-Title=Ratio of $D^+$ to $D^0$ meson yield. 
+Title=Ratio of $D^+$ to $D^0$ meson yield ($|y|<0.5$). 
 XLabel= $p_T$ [GeV/c]
 YLabel=$\sigma(D^+)/\sigma(D^0)$
 ConnectBins=0
@@ -59,7 +59,7 @@ LegendXPos=0.55
 END PLOT
 
 BEGIN PLOT /ALICE_2017_I1511870/d06-x01-y01
-Title=Ratio of $D^{*+}$ to $D^0$ meson yield. 
+Title=Ratio of $D^{*+}$ to $D^0$ meson yield ($|y|<0.5$). 
 XLabel= $p_T$ [GeV/c]
 YLabel=$\sigma(D^{*+})/\sigma(D^0)$
 ConnectBins=0
@@ -71,7 +71,7 @@ LegendXPos=0.55
 END PLOT
 
 BEGIN PLOT /ALICE_2017_I1511870/d07-x01-y01
-Title=Ratio of $D_s^+$ to $D^0$ meson yield. 
+Title=Ratio of $D_s^+$ to $D^0$ meson yield ($|y|<0.5$). 
 XLabel= $p_T$ [GeV/c]
 YLabel=$\sigma(D_s^+)/\sigma(D^0)$
 ConnectBins=0
@@ -83,7 +83,7 @@ LegendXPos=0.55
 END PLOT
 
 BEGIN PLOT /ALICE_2017_I1511870/d08-x01-y01
-Title=Ratio of $D_s^+$ to $D^+$ meson yield. 
+Title=Ratio of $D_s^+$ to $D^+$ meson yield ($|y|<0.5$). 
 XLabel= $p_T$ [GeV/c]
 YLabel=$\sigma(D_s^+)/\sigma(D^+)$
 ConnectBins=0
@@ -146,7 +146,7 @@ LegendXPos=0.55
 END PLOT
 
 BEGIN PLOT /ALICE_2017_I1511870/d13-x01-y01
-Title=Integrated cross sections for prompt $c\overline{c}$ production. 
+Title=Integrated cross sections for prompt $c\overline{c}$ production ($|y|<0.5$). 
 XLabel= $E$ = 7 TeV
 YLabel=$\frac{\text{d}\sigma}{\text{d}y}$ [$\mu$b]
 ConnectBins=0
@@ -158,7 +158,7 @@ LegendXPos=0.55
 END PLOT
 
 BEGIN PLOT /ALICE_2017_I1511870/d14-x01-y01
-Title=Mean $p_T$ of prompt $D^0$. 
+Title=Mean $p_T$ of prompt $D^0$ ($|y|<0.5$). 
 XLabel= $E$ = 7 TeV
 YLabel=$\langle p_T \rangle$ [GeV/c]
 ConnectBins=0
@@ -184,7 +184,7 @@ LegendXPos=0.55
 END PLOT
 
 BEGIN PLOT /ALICE_2017_I1511870/d16-x01-y01
-Title=Integrated cross sections for prompt $c\overline{c}$ full y. 
+Title=Integrated cross sections for prompt $c\overline{c}$ (full y). 
 XLabel= $E$ = 7 TeV
 YLabel=$\frac{\text{d}\sigma}{\text{d}y}$ [$\mu$b]
 ConnectBins=0
