72bf50e15c967ecb0b0421fe889241bab37c2abb
jackal1-66 <mgiacalo@gmail.com>
2022-04-07 19:11:13 +0200

    Edited files for Herwig and fixed bug leading to no ouput in log for RIVET in runHerwig.sh

--- ALICE_2022_I1868463/runHerwig.sh
+++ ALICE_2022_I1868463/runHerwig.sh
@@ -10,13 +10,13 @@ cd $HERWIG_ROOT/share/Herwig
 Herwig read LHC-MBDefault.in
 Herwig read LHC-MB_CRBaryonicRec.in
 Herwig read LHC-MB_CRPlain.in
-Herwig run LHC-MBDefault.run &>$WD/Herwig1e6SOFTDef.log &
-Herwig run LHC-MB_CRPlain.run &>$WD/Herwig1e6CRPlain.log &
-Herwig run LHC-MB_CRBaryonicRec.run &>$WD/Herwig1e6CRBaryonicRec.log &
+Herwig run -N 1000000 LHC-MBDefault.run &>$WD/Herwig1e6SOFTDef.log &
+Herwig run -N 1000000 LHC-MB_CRPlain.run &>$WD/Herwig1e6CRPlain.log &
+Herwig run -N 1000000 LHC-MB_CRBaryonicRec.run &>$WD/Herwig1e6CRBaryonicRec.log &
 cd $WD
 #Start Rivet analysis
-rivet --ignore-beams --pwd -a ALICE_2022_I1868463 /tmp/Herwig7.fifo -o herwig7pp1e6.yoda -n 1000000 >RivetHerwig7pp1e6.log &
-rivet --ignore-beams --pwd -a ALICE_2022_I1868463 /tmp/Herwig7Bar.fifo -o herwig7pp1e6bar.yoda -n 1000000 >RivetHerwig7pp1e6bar.log &
-rivet --ignore-beams --pwd -a ALICE_2022_I1868463 /tmp/Herwig7Plain.fifo -o herwig7pp1e6plain.yoda -n 1000000 >RivetHerwig7pp1e6plain.log &
+rivet --ignore-beams --pwd -a ALICE_2022_I1868463 /tmp/Herwig7.fifo -o herwig7pp1e6.yoda -n 1000000 &>RivetHerwig7pp1e6.log &
+rivet --ignore-beams --pwd -a ALICE_2022_I1868463 /tmp/Herwig7Bar.fifo -o herwig7pp1e6bar.yoda -n 1000000 &>RivetHerwig7pp1e6bar.log &
+rivet --ignore-beams --pwd -a ALICE_2022_I1868463 /tmp/Herwig7Plain.fifo -o herwig7pp1e6plain.yoda -n 1000000 &>RivetHerwig7pp1e6plain.log &
 
 
