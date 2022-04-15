06c631f4c8925df7acffbc926adaa83d7f8c79b7
jackal1-66 <mgiacalo@gmail.com>
2022-04-15 14:16:51 +0200

    Fixed bug in runp8 for ALICE_2017_I1511870

--- ALICE_2017_I1511870/runp8.sh
+++ ALICE_2017_I1511870/runp8.sh
@@ -2,12 +2,12 @@
 
 #Monash
 run-pythia -n 1000000 -o /tmp/p8R3.fifo -e 13000 -c "SoftQCD:all on" &>Pythiapp1e6R3.log &
-rivet --ignore-beams --pwd -a ALICE_2022_I1868463 /tmp/p8R3.fifo -o pythia8pp1e6R3.yoda -n 1000000 &>RivetPythia8pp1e6R3.log &
+rivet --ignore-beams --pwd -a ALICE_2017_I1511870 /tmp/p8R3.fifo -o pythia8pp1e6R3.yoda -n 1000000 &>RivetPythia8pp1e6R3.log &
 #Mode0
 run-pythia -n 1000000 -o /tmp/p80R3.fifo -e 13000 -c "SoftQCD:all on" -i mode0.par &>Pythiapp1e6mode0R3.log &
-rivet --ignore-beams --pwd -a ALICE_2022_I1868463 /tmp/p80R3.fifo -o pythia8pp1e6mode0R3.yoda -n 1000000 &>RivetPythia8pp1e6mode0R3.log &
+rivet --ignore-beams --pwd -a ALICE_2017_I1511870 /tmp/p80R3.fifo -o pythia8pp1e6mode0R3.yoda -n 1000000 &>RivetPythia8pp1e6mode0R3.log &
 #Mode2
 run-pythia -n 1000000 -o /tmp/p82R3.fifo -e 13000 -c "SoftQCD:all on" -i mode2.par &>Pythiapp1e6mode2R3.log &
-rivet --ignore-beams --pwd -a ALICE_2022_I1868463 /tmp/p82R3.fifo -o pythia8pp1e6mode2R3.yoda -n 1000000 &>RivetPythia8pp1e6mode2R3.log &
+rivet --ignore-beams --pwd -a ALICE_2017_I1511870 /tmp/p82R3.fifo -o pythia8pp1e6mode2R3.yoda -n 1000000 &>RivetPythia8pp1e6mode2R3.log &
 
 
